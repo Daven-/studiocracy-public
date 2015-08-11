be4e782e8776a77924447cfaefaaa7e077024fad
r-dc <chhristian.robles@gmail.com>
2015-08-11 10:24:12 -0700

    ¯\_(ツ)_/¯

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,36 +1,16 @@
-jQuery(function() {
+/*jQuery(function() {
   //target comment-form from comment form partial
-  $(".comment-form-div form")
+  $(".comment-form-div")
     .on("ajax:beforeSend", function(evt, xhr, settings) {
       return $(this).find('text_area')
       .addClass('uneditable-input')
       .attr('disabled', 'disabled');
-      console.log("FOUND IT")
     })
     .on("ajax:success", function(evt, data, status, xhr) {
       $(this).find('text_area')
       .removeClass('uneditable-input')
       .removeAttr('disabled', 'disabled')
       .val('');
-      console.log(xhr.responseText);
       return $(xhr.responseText).hide().insertAfter($(this)).show('slow');
     });
-
-
-  /*$(document)
-    .on("ajax:beforeSend", ".comment", function() {
-      return $(this).fadeTo('fast', 0.5); 
-    })
-    .on("ajax:success", ".comment", function() {
-      return $(this).hide('fast');
-    })
-    .on("ajax:error", ".comment", function() {
-      return $(this).fadeTo('fast', 1);
-    });
 });*/
-
-$(document).ready(function(){
-  $('.reply').click(function(){
-    $(this).parent().appendChild('<a>hi</a>')
-  });
-});
--- app/assets/javascripts/reply.js
+++ app/assets/javascripts/reply.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-  $('.reply-button').one('click', function(){
-    var parent_id = $(this).parent('.comment').attr('id')
-    var parent_id_field = "<input value='" + parent_id + "' type='hidden' name='comment[parent_id]'' id='comment_parent_id'>"
+  $('.reply-button')
+    .one('click', function(){
+      var parent_id = $(this).parent('.comment').attr('id')
+      var parent_id_field = "<input value='" + parent_id + "' type='hidden' name='comment[parent_id]'' id='comment_parent_id'>"
 
-    $(this).replaceWith($('#master-comment-form').clone().attr('id', "reply-div-" + parent_id))
-    $('#' + "reply-div-" + parent_id).find('.field').append(parent_id_field)
-  });
+      $(this).replaceWith($('#master-comment-form').clone().attr('id', "reply-div-" + parent_id))
+      $('#' + "reply-div-" + parent_id).find('.field').append(parent_id_field)
+    });
+  
+  /*$(".comment-form-div")
+    .on("ajax:beforeSend", function(evt, xhr, settings) {
+      return $(this).find('text_area')
+      .addClass('uneditable-input')
+      .attr('disabled', 'disabled');
+    })
+    .on("ajax:success", function(evt, data, status, xhr) {
+      $(this).find('text_area')
+      .removeClass('uneditable-input')
+      .removeAttr('disabled', 'disabled')
+      .val('');
+      return $(xhr.responseText).hide().insertAfter($(this)).show('slow');
+    });*/
 });
