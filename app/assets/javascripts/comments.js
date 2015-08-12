44d3a12d858416e53695a0f90ff38a7a0c18497d
Julian <julian.deocampo@gmail.com>
2015-08-11 22:36:48 -0700

    Used delegated event handling for JQuery to work with newly generated comments and reply boxes.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,16 +1,21 @@
-/*jQuery(function() {
-  //target comment-form from comment form partial
-  $(".comment-form-div")
-    .on("ajax:beforeSend", function(evt, xhr, settings) {
-      return $(this).find('text_area')
-      .addClass('uneditable-input')
-      .attr('disabled', 'disabled');
-    })
-    .on("ajax:success", function(evt, data, status, xhr) {
-      $(this).find('text_area')
-      .removeClass('uneditable-input')
-      .removeAttr('disabled', 'disabled')
-      .val('');
-      return $(xhr.responseText).hide().insertAfter($(this)).show('slow');
+$(document).ready(function() {
+    jQuery(function () {
+        //target comment-form from comment form partial
+        $(document)
+            .on("ajax:beforeSend", '.comment-form-div', function (evt, xhr, settings) {
+                console.log("Ajax request sent")
+                return $(this).find('text_area')
+                    .addClass('uneditable-input')
+                    .attr('disabled', 'disabled');
+            })
+            .on("ajax:success", '.comment-form-div', function (evt, data, status, xhr) {
+                console.log("Ajax request successful")
+
+                $(this).find('text_area')
+                    .removeClass('uneditable-input')
+                    .removeAttr('disabled', 'disabled')
+                    .val('');
+                return $(xhr.responseText).hide().insertAfter($(this)).show('slow');
+            });
     });
-});*/
+});
--- app/assets/javascripts/reply.js
+++ app/assets/javascripts/reply.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-  $('.reply-button')
-    .one('click', function(){
-      var parent_id = $(this).parent('.comment').attr('id')
-      var parent_id_field = "<input value='" + parent_id + "' type='hidden' name='comment[parent_id]'' id='comment_parent_id'>"
-
-      $(this).replaceWith($('#master-comment-form').clone().attr('id', "reply-div-" + parent_id))
-      $('#' + "reply-div-" + parent_id).find('.field').append(parent_id_field)
+  $(document)
+    .on('click', '.reply-button', function(){
+          //Check if the box is already active
+          if (!$(this).data('activated')){
+              var parent_id = $(this).parent('.comment').attr('id')
+              //Hidden field for comment parent ID
+              var parent_id_field = "<input value='" + parent_id + "' type='hidden' name='comment[parent_id]'' id='comment_parent_id'>"
+              $(this).replaceWith($('#master-comment-form').clone().attr('id', "reply-div-" + parent_id))
+              $('#' + "reply-div-" + parent_id).find('.field').append(parent_id_field)
+              //Set activated Jquery data to true
+              $(this).data('activated', true)
+          }
     });
   
   /*$(".comment-form-div")
