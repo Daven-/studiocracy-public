22b720c87377a57689a2a712432ec136997aeefc
Julian <julian.deocampo@gmail.com>
2015-08-11 23:07:48 -0700

    remove comment box on submit

--- app/assets/javascripts/reply.js
+++ app/assets/javascripts/reply.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
               var parent_id = $(this).parent('.comment').attr('id')
               //Hidden field for comment parent ID
               var parent_id_field = "<input value='" + parent_id + "' type='hidden' name='comment[parent_id]'' id='comment_parent_id'>"
-              $(this).replaceWith($('#master-comment-form').clone().attr('id', "reply-div-" + parent_id))
+              $(this).replaceWith($('#master-comment-form').clone().attr('id', "reply-div-" + parent_id).show(0))
               $('#' + "reply-div-" + parent_id).find('.field').append(parent_id_field)
               //Set activated Jquery data to true
               $(this).data('activated', true)
