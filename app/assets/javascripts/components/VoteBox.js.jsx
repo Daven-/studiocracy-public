8ef61145097be8837aca81efa073b89365e91520
Julian <julian.deocampo@gmail.com>
2015-08-16 15:59:36 -0700

    began react implementation of votebox component

---
+++ app/assets/javascripts/components/VoteBox.js.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by jd on 8/16/15.
+ */
+
+var VoteBox = React.createClass({
+   render: function() {
+       return (
+       <div className="voteBox">
+           <div className='num_votes'>
+               Test
+           </div>
+       </div>
+       );
+   }
+});
