c39fc10cb8cfd5d83068213aad4137edddb643d3
Julian <julian.deocampo@gmail.com>
2015-08-20 20:53:12 -0700

    some landing page cleanup

--- landing/mailer.php
+++ landing/mailer.php
@@ -1,155 +1,47 @@
 <?php
 
-if(isset($_POST['email'])) {
 
-
-
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-
-    $email_to = "mail@studiocracy.com";
-
-    $email_subject = "Your email subject line";
-
-
-
-
-
-    function died($error) {
-
-        // your error code can go here
-
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-
-        echo "These errors appear below.<br /><br />";
-
-        echo $error."<br /><br />";
-
-        echo "Please go back and fix these errors.<br /><br />";
-
-        die();
-
-    }
-
-
-
-    // validation expected data exists
-
-    if(!isset($_POST['first_name']) ||
-
-        !isset($_POST['last_name']) ||
-
-        !isset($_POST['email']) ||
-
-        !isset($_POST['telephone']) ||
-
-        !isset($_POST['comments'])) {
-
-        died('We are sorry, but there appears to be a problem with the form you submitted.');
-
-    }
-
-
-
-    $first_name = $_POST['first_name']; // required
-
-    $last_name = $_POST['last_name']; // required
-
-    $email_from = $_POST['email']; // required
-
-    $telephone = $_POST['telephone']; // not required
-
-    $comments = $_POST['comments']; // required
-
-
-
-    $error_message = "";
-
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-  if(!preg_match($email_exp,$email_from)) {
-
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-
-  }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-  if(!preg_match($string_exp,$first_name)) {
-
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-
-  }
-
-  if(!preg_match($string_exp,$last_name)) {
-
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-
-  }
-
-  if(strlen($comments) < 2) {
-
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-
-  }
-
-  if(strlen($error_message) > 0) {
-
-    died($error_message);
-
-  }
-
-    $email_message = "Form details below.\n\n";
-
-
-
-    function clean_string($string) {
-
-      $bad = array("content-type","bcc:","to:","cc:","href");
-
-      return str_replace($bad,"",$string);
-
-    }
-
-
-
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-
-    $email_message .= "Comments: ".clean_string($comments)."\n";
-
-
-
-
-
-// create email headers
-
-$headers = 'From: '.$email_from."\r\n".
-
-'Reply-To: '.$email_from."\r\n" .
-
-'X-Mailer: PHP/' . phpversion();
-
-@mail($email_to, $email_subject, $email_message, $headers);
-
-?>
-
-
-
-<!-- include your own success html here -->
-
-
-
-Thank you for contacting us. We will be in touch with you very soon.
-
-
-
-<?php
-
+/*
+$fname = htmlspecialchars($_POST['fname']);
+$lname = htmlspecialchars($_POST['lname']);
+$email = htmlspecialchars($_POST['email']);
+$body = htmlspecialchars($_POST['comments']);
+*/
+
+// send text messages
+require_once 'swift/lib/swift_required.php';
+
+$fName = htmlspecialchars($_POST['fname']);
+$lName = htmlspecialchars($_POST['lname']);
+$body = htmlspecialchars($_POST['comments']);
+$email = htmlspecialchars($_POST['email']);
+
+$name = $fName . " " . $lName;
+
+if (isset($body) && isset($email) && isset($fName) && isset($lName)) {
+	try {
+		$transport = Swift_SmtpTransport::newInstance('studiocracy.com', 465, "ssl")
+		  ->setUsername('test@studiocracy.com')
+		  ->setPassword('test123');
+
+		$mailer = Swift_Mailer::newInstance($transport);
+
+		$message = Swift_Message::newInstance("Inquiry from " . $name)
+			->setFrom(array($email => $name))
+			->setTo(array('test@studiocracy.com'))
+			->setBody($body);
+
+		$result = $mailer->send($message);
+
+		echo "Message successfully sent!";
+	} catch (Exception $e) {
+		echo "Message failed to send.";
+	}
+} else {
+	$response = "There was an error sending your email. Please fill in all boxes.";
+
+	echo $response;
 }
 
+//echo $name . " " . $body . " " . $email;
 ?>
